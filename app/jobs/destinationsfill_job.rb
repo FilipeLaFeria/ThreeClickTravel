afebe830faaf16a3f5f03d3160ef8d4b45acb412
Jacome Saavedra <jacomebsaavedra@gmail.com>
2021-06-10 15:39:12 +0100

    Adjustment on names of columns

--- app/jobs/destinationsfill_job.rb
+++ app/jobs/destinationsfill_job.rb
@@ -7,7 +7,7 @@ class DestinationsfillJob < ApplicationJob
     puts "Creating destinations..."
     @flights = Flight.where('price <= ?', budget * 0.4).where('start_date = ?', start_date)
     @flights.each do |flight|
-      @accommodations = Accommodation.where('city_name = ?', flight.destination_city).where('check_in = ?', flight.start_date)
+      @accommodations = Accommodation.where('city_name = ?', flight.city_name).where('check_in = ?', flight.start_date)
       @accommodations.each do |accommodation|
         Destination.create(flight: flight, accommodation: accommodation)
       end
