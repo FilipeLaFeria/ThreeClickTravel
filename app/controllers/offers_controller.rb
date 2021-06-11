b9612069b5c0c15c67c954ae805927ebe4676a1f
Filipe La Feria <filipe.laferia@gmail.com>
2021-06-11 11:55:12 +0100

    controllers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,5 @@
 class OffersController < ApplicationController
+  before_action :generate_offers, only: :index
   def index
     @offers = Offer.all
 
@@ -9,6 +10,14 @@ class OffersController < ApplicationController
         info_window: render_to_string(partial: 'info_window', locals: { offer: offer }),
         # image_url: helpers.asset_url('logo.png')
       }
+    end
+  end
+  private
+
+  def generate_offers
+    @destination = destination.find
+    3.times do
+      Offer.create(total_price: soma dos preços, destination: dest)
     end
   end
 end
