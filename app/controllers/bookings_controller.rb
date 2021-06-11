16c9a7ed8601e0a25833cfc3914c612fb469d4f5
Filipe La Feria <filipe.laferia@gmail.com>
2021-06-11 12:41:41 +0100

    updates

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,25 +1,3 @@
 class BookingsController < ApplicationController
-  #before_action :set_booking, only: %i[show destroy]
-
-  #def index
-  #end
-
-  #def show
-  #end
-
-  #def create
-
-  #end
-
-  #def delete
-   # booking.destroy
-    #redirect_to path?????, notice: "Your booking was deleted successfully!"
-  #end
-
- #private
-
-  #def set_booking
-   # @booking = Bookings.find(params[:id])
-  #end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_150809) do
+ActiveRecord::Schema.define(version: 2021_06_11_113925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_150809) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "city_name"
+    t.text "country_name"
     t.index ["accommodation_id"], name: "index_destinations_on_accommodation_id"
     t.index ["flight_id"], name: "index_destinations_on_flight_id"
   end
