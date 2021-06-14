1c0082af783dbdec5276f4c878ce74f62593d644
Filipe La Feria <filipe.laferia@gmail.com>
2021-06-14 14:13:32 +0100

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,15 @@ Rails.application.routes.draw do
 
   resources :definitions, only: %i[show new create update destroy]
 
-  resources :bookings, only: %i[index show new create destroy]
-
   resources :ratings, only: %i[show new create update destroy]
 
   resources :destinations, only: %i[index] do
-    resources :offers, only: %i[index show]
+    resources :offers, only: %i[index show] do
+      resources :bookings, only: %i[show new create destroy]
+    end
   end
 
-  resources :accomodations, only: %i[new create]
+  # resources :accomodations, only: %i[new create]
 
   resources :flights, only: %i[new create]
  end
