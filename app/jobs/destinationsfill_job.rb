394a4d0370945213a5f69a0eba2c6345e22c00b9
Jacome Saavedra <jacomebsaavedra@gmail.com>
2021-06-15 14:40:49 +0100

    Correction

--- app/jobs/destinationsfill_job.rb
+++ app/jobs/destinationsfill_job.rb
@@ -1,16 +1,17 @@
 class DestinationsfillJob < ApplicationJob
   queue_as :default
 
-  def perform(budget, start_date)
+  def perform(budget, start_date, end_date)
     puts 'Deleting old destinations...'
     Destination.destroy_all
     puts 'Creating destinations...'
-    @flights = Flight.where('price <= ?', budget * 0.4).where('start_date = ?', start_date)
+    @flights = Flight.where('start_date = ?', start_date).where('end_date = ?', end_date)
     @flights.each do |flight|
-      @accommodations = Accommodation.where('price <= ?', budget * 0.6).where('city_name = ?', flight.city_name).where('check_in = ?', flight.start_date)
+      @accommodations = Accommodation.where('city_name = ?', flight.city_name).where('check_in = ?', start_date).where('check_out = ?', end_date)
       @accommodations.each do |accommodation|
-        Destination.create(flight: flight, accommodation: accommodation, city_name: flight.city_name, country_name: flight.country_name)
+        Destination.create(flight: flight, accommodation: accommodation, city_name: flight.city_name, country_name: flight.country_name, total_price: flight.price + accommodation.price)
       end
     end
   end
 end
+
